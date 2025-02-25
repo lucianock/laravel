b3c00bf6e495f8b3d7aba2d651145e86beccdc73
Luciano <62312023+lucianock@users.noreply.github.com>
2025-02-25 17:24:34 -0300

    install argon

--- breeze/database/seeders/DatabaseSeeder.php
+++ breeze/database/seeders/DatabaseSeeder.php
@@ -2,33 +2,25 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        $this->call([
-            MarcaSeeder::class,
-            CategoriaSeeder::class,
-            ProductoSeeder::class,
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'firstname' => 'Admin',
+            'lastname' => 'Admin',
+            'email' => 'admin@argon.com',
+            'password' => bcrypt('secret')
         ]);
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        /*
-        $this->call([
-            City::class,
-            State::class,
-            Property::class,
-        ]);
-        */
     }
 }
